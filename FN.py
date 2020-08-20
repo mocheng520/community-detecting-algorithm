62fbddadc38782a9c459e7029c17523bedeafdd2
僵尸王 <65437937+mocheng520@users.noreply.github.com>
2020-08-20 22:29:24 +0800

    👴巨菜

--- FN.py
+++ FN.py
@@ -1,90 +1,71 @@
-
-
 import networkx as nx
 import time
 
 class FN(object):
     def __init__(self,G):
-        self.G =nx.Graph()
-        self.firstgraph = G
+        self.firstgraph = G.copy()
+        self.G = nx.Graph()
+        self.realG = G.copy()
+        self.Q = 0
         self.part = []
         self.finapart = []
-        self.used_edge = []
-        self.finaQ =0
-        self.Q = 0
 
-    def pre(self,G):
-        self.G.add_nodes_from([i for i in G.nodes()])
-    def updata(self,G):
-        self.pre(G)
-        maxcomponent =[]
-        maxG = self.G
-        maxedge = list(self.firstgraph.edges())
 
-        for num in range(len(self.firstgraph.edges())):
-            print('已完成%d/%d'%(num+1,len(self.firstgraph.edges())))
-            maxq = -99
+
+    def updata(self):
+
+        while len(self.firstgraph.edges())>0:
+
+            maxq = -999999999
+            maxedge = []
+            maxcompose = []
 
             for i in self.firstgraph.edges():
-                if not ( list(i) in self.used_edge):#这里可以用字典优化下，可以把O（N）优化到O（1），有空再来⛏吧（2020.8.19/20:54）
+                Gclone = self.G.copy()
+                Gclone.add_edge(i[0],i[1])
 
-                    Gclone = self.G.copy()#一定要G.copy（），因为python传引用,翻了一天的资料，差点怀疑人生，最后发现这里错了（2020.8.19/20:10）
+                q = self.calcQ(Gclone,[list(c) for c in list(nx.connected_components(Gclone))])
 
-                    Gclone.add_edge(list(i)[0],list(i)[1])
-
-
-                    component = [list(c) for c in list(nx.connected_components(Gclone))]
-                    q = self.calcQ(Gclone,component)
-
-                    if q >= maxq:
-                        maxq = q
-                        maxcomponent = component
-                        maxedge = list(i)
-
-
-            self.used_edge.append(maxedge)
-
+                if q > maxq:
+                    maxedge = i
+                    maxq = q
+                    maxcompose = [list(c) for c in list(nx.connected_components(Gclone))]
+            print(len(self.G.edges()))
             self.G.add_edge(maxedge[0],maxedge[1])
+            self.firstgraph.remove_edge(maxedge[0],maxedge[1])
 
-            if maxq >self.finaQ:
-                    self.finaQ = maxq
-                    self.finapart = maxcomponent
-
-
-
+            if maxq >self.Q:
+                self.Q = maxq
+                self.part = maxcompose
 
 
 
 
-    def calcQ(self,Gclone,component):
+
+    def calcQ(self,G,comp):
         m = len(self.firstgraph.edges())
         a = []
         e = []
 
-
-
-
-        for community in component:
-            t = 0.0
+        for community in comp:
+            t = 0
             for node in community:
-                t += self.firstgraph.degree(node)
+
+                t += self.realG.degree(node)
             a.append(t / float(2 * m))
-        for community in component:
+        for community in comp:
             t = 0.0
             for i in community:
                 for j in community:
-                    if self.firstgraph.has_edge(i, j):
+                    if self.realG.has_edge(i, j):
                         t += 1.0
             e.append(t / float(2 * m))
 
         q = 0.0
         for ei, ai in zip(e, a):
-
             q += (ei - ai ** 2)
 
-        return q
-
-
+        return  q
 
 if __name__ =="__main__":
     G=nx.read_gml('football真.gml')
@@ -93,13 +74,11 @@ if __name__ =="__main__":
 
     a= FN(G)
 
-    a.updata(G)
+    a.updata()
     print('------------')
 
-    print('模块度为：%d'%(a.finaQ))
+    print('模块度为',(a.Q))
     print('time:%d'%(time.time()-start))
-    print('一共%d个社团' % (len(a.finapart)))
-    for i in a.finapart:
+    print('一共%d个社团' % (len(a.part)))
+    for i in a.part:
         print(i)
-
-
